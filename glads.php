c70355278a629857d0751226ce6b195fc99adc79
terciotales <terciotalesb@gmail.com>
2025-02-26 09:56:15 -0300

    :sparkles: feat: update main plugin file

--- glads.php
+++ glads.php
@@ -21,17 +21,60 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-
-/**
- * Returns the main instance of Glads to prevent the need to use globals.
- *
- * @since  1.0.0
- * @return object WordPress_Plugin_Template
- */
-function glads(): object {
-
-
-    return (object)[];
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
 }
 
-glads();
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'GLADS_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-glads-activator.php
+ */
+function activate_glads() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-glads-activator.php';
+    Glads_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-glads-deactivator.php
+ */
+function deactivate_glads() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-glads-deactivator.php';
+    Glads_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_glads' );
+register_deactivation_hook( __FILE__, 'deactivate_glads' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-glads.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_glads() {
+
+    $plugin = new Glads();
+    $plugin->run();
+
+}
+
+run_glads();
