b76ef264d1d5cfcaebe847f4461a2eb1e735c556
terciotales <terciotalesb@gmail.com>
2025-02-26 15:17:57 -0300

    :bug: fix: error to run webpack solved

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,25 @@
 const path = require('path');
-const glob = require('glob');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 // Adiciona um novo loader nas regras do scss para fazer includes em massa usando "*"
-const customRules = defaultConfig.module.rules.map((item) => {
-	if (item.test && item.test.test('.scss')) {
-		item.use = [...item.use, {loader: 'import-glob-loader'}];
+const customRules = defaultConfig.module.rules.map( ( item ) => {
+	if ( item.test && item.test.test( '.scss' ) ) {
+		item.use = [ ...item.use, { loader: 'import-glob-loader' } ];
 	}
 
 	return item;
-});
+} );
 
 module.exports = {
 	...defaultConfig,
 	entry: {
 		...defaultConfig.entry,
-		'glads-admin': glob.sync('./assets/admin/js/*.js'),
-		'glads-public': glob.sync('./assets/public/js/*.js'),
-		'glads-admin-style': glob.sync('./assets/admin/css/*.scss'),
-		'glads-public-style': glob.sync('./assets/public/css/*.scss'),
-		'glads-ad-block': glob.sync('./includes/blocks/ad/index.js'),
-		'glads-ad-block-view': glob.sync('./includes/blocks/ad/view.js'),
-		'glads-ad-block-view-style': glob.sync('./includes/blocks/ad/style.scss'),
+		admin: path.resolve( __dirname, 'src/admin/index.js' ),
+		public: path.resolve( __dirname, 'src/public/index.js' ),
 	},
 	output: {
 		...defaultConfig.output,
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(process.cwd(), 'dist'),
 		publicPath: './',
 	},
 	module: {
